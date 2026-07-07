e1764219a33c7cc3fd0697c27e506b04bd8cf835
test23416 <3225795071@qq.com>
2026-07-07 20:16:19 +0800

    add simple frontend chat page

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
 
 
 app = FastAPI(title = "SJTU Freshman Agent Learning")
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
